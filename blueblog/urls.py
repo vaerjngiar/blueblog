e7f6d4afcd2fd1de691c0ba6a58a3186b856edc6
vaerjngiar <vaerjngiar@gmail.com>
2017-05-26 10:59:24 +0300

    first auth added

---
+++ blueblog/urls.py
@@ -0,0 +1,31 @@
+"""blueblog URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.10/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import include
+from django.conf.urls import url
+from django.contrib import admin
+from django.views.generic import TemplateView
+from accounts.views import UserRegistrationView
+from django.contrib.auth.views import login
+from django.contrib.auth.views import logout
+
+
+urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', TemplateView.as_view(template_name='base.html'),name='home'),
+    url(r'^new-user/$', UserRegistrationView.as_view(), name='user_registration'),
+    url(r'^login/$', login, {'template_name': 'accounts/login.html'}, name='login'),
+    url(r'^logout/$', logout, {'next_page': '/login/'}, name='logout'),
+]
